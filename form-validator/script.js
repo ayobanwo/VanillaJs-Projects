8bd00483fc052ee83f41a5828ac362f8de9ce8df
ayobanwo <olubanwoayomide@gmail.com>
2022-01-10 15:18:04 +0100

    new commit

--- form-validator/script.js
+++ form-validator/script.js
@@ -19,36 +19,67 @@ function showSuccess(input) {
 }
 
 //Validate Email
-function validEmail(email) {
-    return String(email)
-    .toLowerCase()
-    .match(
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    ); 
-}
-form.addEventListener('submit', function(e){
-    e.preventDefault();
-    if(username.value === ''){
-        showError(username, 'Username required');
-    }else{
-        showSuccess(username)
-    }
-    if(email.value === ''){
-        showError(email, 'Email required');
-    }else if(!validEmail(email.value)){
+function checkEmail(email) {
+    if (String(email.value.trim())
+            .toLowerCase()
+            .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            )
+    ){
+        showSuccess(email);
+    } 
+    else{
         showError(email, 'Email is not valid');
     }
+}
+
+//Check password match
+function checkPasswordMatch(pass1, pass2) {
+    if(pass1.value === pass2.value){
+        showSuccess(pass1);
+    }
     else{
-        showSuccess(email)
+        showError(pass2, 'Passwords do not match')
     }
-    if(password.value === ''){
-        showError(password, 'Password required');
-    }else{
-        showSuccess(password)
+}
+
+// Check required fields
+function checkRequired(inputArr) {
+    inputArr.forEach(function (input) {
+        if(input.value.trim() === ''){
+            showError(input, `${fieldName(input)} field required` )
+        }
+        else{
+            showSuccess(input);
+        }
+    })
+}
+
+// Get field name
+function fieldName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+// Check lenght of field
+function checkLength(input, min, max) {
+    if(input.value.trim().length < min){
+        showError(input, `${fieldName(input)} must be at least ${min} characters`)
     }
-    if(password2.value === ''){
-        showError(password2, 'Password required');
-    }else{
-        showSuccess(password2)
+    else if (input.value.trim().length > max ){
+        showError(input, `${fieldName(input)} must be less than ${max} characters`)
     }
+    else{
+        showSuccess(input);
+    }
+}
+
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    checkRequired([username, email, password, password2]);
+    checkLength(username, 3, 15);
+    checkLength(password, 8, 20);
+    checkEmail(email);
+    checkPasswordMatch(password, password2);
 })
