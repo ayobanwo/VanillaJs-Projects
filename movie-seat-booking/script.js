a2f0dffa495039e1b99b829288213873827a77b3
ayobanwo <olubanwoayomide@gmail.com>
2022-01-24 00:20:56 +0100

    new commit

--- movie-seat-booking/script.js
+++ movie-seat-booking/script.js
@@ -4,25 +4,54 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
+loadSavedData();
+
 let ticketPrice = +movieSelect.value;
 
+//Get data from local storage 
+function  loadSavedData() {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+    if (selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if(selectedSeats.indexOf(index) > -1){
+                seat.classList.add('selected');
+            }
+        });
+    }
+    
+    const movieIndex = localStorage.getItem('movieIndex');
+    if (movieIndex !== null)  {
+        movieSelect.selectedIndex = movieIndex;
+    }
+
+}
+
+//Store movie data to local storage
+function setMovieData(movie, price){
+    localStorage.setItem('moviePrice' , (price));
+    localStorage.setItem('movieIndex', movie);
+}
 // update count of selected seats
 function  updateSelectedCount() {
     const selectedSeats  = document.querySelectorAll('.row .seat.selected');
 
+    
     const seatsIndex = [...selectedSeats].map(seat => 
         [...seats].indexOf(seat) 
-    );
+    ); 
 
-    console.log(seatsIndex);
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
     const selectedSeatsCount = selectedSeats.length;
     count.innerText = selectedSeatsCount;
-    total .innerText = selectedSeatsCount * ticketPrice;
+    total.innerText = selectedSeatsCount * ticketPrice;
 }
 
 //Movie Select
 movieSelect.addEventListener('change', e => {
-    ticketPrice = +e.target.value;
+    ticketPrice = +e.target.value; 
+    setMovieData(e.target.selectedIndex, e.target.value);
     updateSelectedCount();
 })
 
@@ -36,3 +65,4 @@ container.addEventListener('click', e => {
         updateSelectedCount();
     }
 });
+updateSelectedCount();
